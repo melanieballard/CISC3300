c00932d171d7e5886dde9121f28d5a0d0f7c81b1
melanieballard <melanieb.1379@gmail.com>
2024-03-16 16:59:49 -0400

    new

--- HW8/PHP/app/models/Post.php
+++ HW8/PHP/app/models/Post.php
@@ -4,11 +4,39 @@ namespace app\models;
 
 class Post
 {
-    protected static $posts = [];
+    public static $posts = [];
 
-    public static function savePost($newPost){
-       self::$posts[] = $newPost;
+    //public static function savePost($newPost){
+      // self::$posts[] = $newPost;
+    //}
+
+    public static function savePost($_POST) {
+        // Retrieve form data
+        $name = $_POST['name'] ?? null;
+        $description = $_POST['description'] ?? null;
+
+        if (!$name || !$description) {
+            http_response_code(400);
+            echo "Error: Name and description are required.";
+            return;
+        }
+
+        // Sanitize data if needed
+        $name = htmlspecialchars($name);
+        $description = htmlspecialchars($description);
+
+        $date = date('His');
+
+        // Store data in class variable
+        $newPost = [
+            'id' => $date,
+            'name' => $name,
+            'description' => $description
+        ];
+
+        self::$posts[] = $newPost;
     }
+
 
     public static function getPosts() {
         return self::$posts;
