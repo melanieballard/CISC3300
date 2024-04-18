65f890e46ed819bba2a6c0f43201738ccfb2b630
melanieballard <melanieb.1379@gmail.com>
2024-04-18 16:32:09 -0400

    new

--- FinalProject/public/assets/views/spotify/recPlaylist.php
+++ FinalProject/public/assets/views/spotify/recPlaylist.php
@@ -6,11 +6,15 @@
         <link rel="stylesheet" type="text/css" href="assets/styles/mainpageStyle.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="assets/js/newPlaylist.js"></script>
         <title>Reccomendations</title>
     </head>
     <body class = "container-fluid h-100 d-grid place-items-center">
         <div class = "container">
             <h1>New Playlist</h1>
+            <div id="trackList">
+
+            </div>
             
         </div>
     </body>
