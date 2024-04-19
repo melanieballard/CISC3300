b0524b47da57b9ef9b7eb2917b40d544fd9da8b1
melanieballard <melanieb.1379@gmail.com>
2024-04-19 19:53:23 -0400

    final

--- FinalProject/public/assets/views/main/notFound.php
+++ FinalProject/public/assets/views/main/notFound.php
@@ -3,22 +3,22 @@
     <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" type = "text/css" href="assets/styles/mainpageStyle.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" type = "text/css" href="assets/styles/mainpageStyle.css">
         <title>Page Not Found</title>
     </head>
-    <body class = "container-fluid h-100 d-grid place-items-center">
-        <div class = "container">
+    <body class = "container-fluid h-100 d-flex justify-content-center align-items-center p-0">
+        <div class = "center w-100">
             <div class = "center">
                 <div class = "row justify-content-center">
-                    <div id = "header" class = "col-md-4 text-center mt-4 box">
+                    <div id = "header" class = "col-md-8 text-center mt-4 box">
                         <h1>404 Not Found</h1>
                         <h4>Oops, it looks like the page you are looking for doesn't exist!</h4>
                     </div>
                 </div>
                 <div class = "row justify-content-center mt-2">
-                    <div id = "content" class = "col-md-4 text-center mt-2 box">
-                        <button id = 'go'><a id = 'go' href = "/">Home</a></button>
+                    <div id = "content" class = "col-md-8 text-center mt-2 box">
+                        <a id = 'go' href = "/"><button id = 'go'>Home</button></a>
                     </div>
                 </div>
             </div>
