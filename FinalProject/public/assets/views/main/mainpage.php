ddd92872f04c240e546c8d453797b3cbd44be7c9
melanieballard <melanieb.1379@gmail.com>
2024-07-01 13:04:11 -0400

    new

--- FinalProject/public/assets/views/main/mainpage.php
+++ FinalProject/public/assets/views/main/mainpage.php
@@ -4,38 +4,43 @@
         <meta charset="UTF-8">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" type = "text/css" href="assets/styles/homepageStyle.css"> 
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/mainpage.js"></script>
         <title>Melanie Ballard</title>
     </head>
-    <body class = "container-fluid h-100 d-flex justify-content-start align-items-start p-0">
-        <div class="pos-f-t">
-            <div class="collapse" id="navbarToggleExternalContent">
-                <div class="bg-dark p-4">
-                    <h4 class="text-white">Collapsed content</h4>
-                    <span class="text-muted">Toggleable via the navbar brand.</span>
-                </div>
-            </div>
-            <nav class="navbar w-100 sticky-top navbar-expand-sm navbar-dark bg-dark">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+    <body class = "container-fluid h-100 d-flex p-0">
+        <nav class="navbar w-100 sticky-top navbar-expand-md navbar-light px-3" style="background-color: #d9c0f0;">
+                <a class="navbar-brand" href="#">Melanie Ballard</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarText">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" href="/">Home</a>
+                            <a class="nav-link" href="#">Resume</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="">Resume</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">About Me</a>
+                            <a class="nav-link" href="#">About Me</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/playlist-generator">Spotify Playlist Generator</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.depop.com/melb2123/">Depop</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact Me</a>
+                        </li>
                     </ul>
                 </div>
-            </nav>
-        </div>
+                <span class="navbar-text">
+                    <a href="https://github.com/melanieballard"><img src="assets/styles/images/github.png" width="30" height="30" alt="Github Link"></a>
+                    <a href="www.linkedin.com/in/melanieballard"><img src="assets/styles/images/linkedin.png" width="30" height="30" alt="Linkedin Link"></a>
+                </span>
+
+        </nav>
     </body>
 
 </html>
