66ac821e9fe75e3d8ff38210de6718198db741b5
melanieballard <melanieb.1379@gmail.com>
2024-07-02 12:15:01 -0400

    new

--- FinalProject/public/assets/views/main/mainpage.php
+++ FinalProject/public/assets/views/main/mainpage.php
@@ -2,6 +2,7 @@
 <html lang = "en">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" type = "text/css" href="assets/styles/homepageStyle.css"> 
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
@@ -10,37 +11,53 @@
         <script src="assets/js/mainpage.js"></script>
         <title>Melanie Ballard</title>
     </head>
-    <body class = "container-fluid h-100 d-flex p-0">
-        <nav class="navbar w-100 sticky-top navbar-expand-md navbar-light px-3" style="background-color: #d9c0f0;">
-                <a class="navbar-brand" href="#">Melanie Ballard</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Resume</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About Me</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/playlist-generator">Spotify Playlist Generator</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://www.depop.com/melb2123/">Depop</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact Me</a>
-                        </li>
-                    </ul>
+    <body class = "container-fluid h-100 d-flex flex-column p-0">
+        <div class = "d-flex">
+            <nav class="navbar w-100 sticky-top navbar-expand-lg navbar-light px-3" style="background-color: #d9c0f0;">   
+                <div class="container-fluid px-1">
+                    <a class="navbar-brand m-0" href="#">Melanie Ballard</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarText">
+                        <ul class="navbar-nav">
+                            <li class="nav-item px-1">
+                                <a class="nav-link" href="#">Resume</a>
+                            </li>
+                            <li class="nav-item px-1">
+                                <a class="nav-link" href="#">About Me</a>
+                            </li>
+                            <li class="nav-item px-1">
+                                <a class="nav-link" href="/playlist-generator">Spotify Playlist Generator</a>
+                            </li>
+                            <li class="nav-item px-1">
+                                <a class="nav-link" href="https://www.depop.com/melb2123/">Depop</a>
+                            </li>
+                            <li class="nav-item px-1">
+                                <a class="nav-link" href="#">Contact Me</a>
+                            </li>
+                        </ul>
+                        <span class="navbar-text">
+                            <a href="https://github.com/melanieballard"><img src="assets/styles/images/github.png" width="40" height="30" alt="Github Link" class="px-1"></a>
+                            <a href="www.linkedin.com/in/melanieballard"><img src="assets/styles/images/linkedin.png" width="40" height="30" alt="Linkedin Link" class = "px-1"></a>
+                        </span>
+                    </div>
                 </div>
-                <span class="navbar-text">
-                    <a href="https://github.com/melanieballard"><img src="assets/styles/images/github.png" width="30" height="30" alt="Github Link"></a>
-                    <a href="www.linkedin.com/in/melanieballard"><img src="assets/styles/images/linkedin.png" width="30" height="30" alt="Linkedin Link"></a>
-                </span>
+            </nav>
+        </div>
 
-        </nav>
+        <div class="container-fluid d-flex col-9 mt-5 mb-2 pt-5 justify-content-center">
+            <h1 class="text-center">Hello!</h1>
+        </div>
+        <div class="container-fluid d-flex col-9 col-md-5 m-auto justify-content-center">         
+            <p class="text-center">My name is Melanie and I am an aspiring Software Engineer studying Computer Science and English 
+                at Fordham University. This website is a one-stop spot for all of my personal projects as well as some 
+                other fun facts about me! Feel free to use the Contact Me page to send me an email with any questions. 
+            </p>
+        </div>
+
+        <div class="container-fluid d-flex col-9 mt-5 justify-content-center">
+            <img src="assets/styles/images/me.jpeg" width="300" height="403" alt="Me!">
+        </div>
     </body>
-
 </html>
